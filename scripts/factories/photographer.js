4144733f9b526303fea3214981c968f0d25ec3b7
Richard Ogden <ogden.richard@gmail.com>
2023-03-27 16:02:05 +0200

    27032023 | dom elements#2 + aria start + tabindex

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,10 +5,20 @@ function photographerFactory(data) {
 
   function getUserCardDOM() {
     const article = document.createElement('article');
+    article.ariaLabel = `photographe ${name}`;
+    article.id = `${id}`;
     const figure = document.createElement('figure');
+    figure.setAttribute('aria-labelledby', article.id);
     const figcaption = document.createElement('figcaption');
+    figcaption.ariaLabel = `informations sur ${name}`;
+    const linkContainer = document.createElement('div');
+    linkContainer.classList.add('photographer__link');
+    linkContainer.role = 'link';
+    linkContainer.ariaLabel = `lien vers la page de ${name}`;
+    linkContainer.setAttribute('tabindex', '0');
     const img = document.createElement('img');
     img.setAttribute('src', picture);
+    img.setAttribute('alt', `portrait de ${name}`);
     const h2 = document.createElement('h2');
     h2.textContent = name;
     const h3 = document.createElement('h3');
@@ -20,8 +30,9 @@ function photographerFactory(data) {
     pPrice.classList.add('photographer__price');
     pPrice.textContent = `${price}€/jour`;
     article.appendChild(figure);
-    figure.appendChild(img);
-    figure.appendChild(h2);
+    figure.appendChild(linkContainer);
+    linkContainer.appendChild(img);
+    linkContainer.appendChild(h2);
     figure.appendChild(figcaption);
     figcaption.appendChild(h3);
     figcaption.appendChild(pTag);
