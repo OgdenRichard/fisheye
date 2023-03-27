14311665c8870f570b0809da63ebfdb4f43caee8
Richard Ogden <ogden.richard@gmail.com>
2023-03-27 11:27:53 +0200

    27032023 | fetch photographers + css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,20 @@
 function photographerFactory(data) {
-  const { name, portrait } = data;
+  const { id, name, portrait } = data;
 
-  const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/images/photographers/compressed/${id}/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement('article');
+    const figure = document.createElement('figure');
+    const figcaption = document.createElement('figcaption');
     const img = document.createElement('img');
     img.setAttribute('src', picture);
     const h2 = document.createElement('h2');
     h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    article.appendChild(figure);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    figcaption.appendChild(h2);
     return article;
   }
   return { name, picture, getUserCardDOM };
