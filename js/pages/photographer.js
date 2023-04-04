59d80c45f746cab7c9e041a01ec2ff1b3736875c
Richard Ogden <ogden.richard@gmail.com>
2023-04-04 11:47:54 +0200

    04042023 | filter photographer.js

--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -1,1 +1,28 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographer() {
+  const params = new URL(document.location).searchParams;
+  const id = parseInt(params.get('photographer_id'), 10);
+  const response = await fetch('./data/photographers.json');
+  const jsonData = await response.json();
+  return {
+    photographer: {
+      ...jsonData.photographers.filter(
+        (photographer) => photographer.id === id
+      ),
+    },
+    media: [...jsonData.media.filter((media) => media.photographerId === id)],
+  };
+}
+
+async function display(photographer) {
+  console.log(photographer);
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographer, media } = await getPhotographer();
+  console.log(photographer);
+  console.log(media);
+  // display(photographer);
+}
+
+init();
