9a816ddd3522dd6282b0d19a5c1bc577dcc8e922
Richard Ogden <ogden.richard@gmail.com>
2023-04-06 11:15:27 +0200

    05042023 | class MediaModel

---
+++ js/models/MediaModel.js
@@ -0,0 +1,40 @@
+export default class MediaModel {
+  constructor(media) {
+    this.id = media.id;
+    this.photographerId = media.photographerId;
+    this.title = media.title;
+    this.likes = media.likes;
+    this.date = media.date;
+    this.price = media.price;
+    this.type(media);
+    this.filename(media);
+  }
+
+  get type() {
+    return this.mediaType;
+  }
+
+  set type(media) {
+    if (media.image) {
+      this.mediaType = 'picture';
+    } else if (media.video) {
+      this.mediaType = 'video';
+    } else {
+      this.mediaType = null;
+      // TODO throw error
+    }
+  }
+
+  /**
+   * @param {(arg0: any) => void} media
+   */
+  set filename(media) {
+    if (this.mediaType) {
+      this.file = this.mediaType === 'picture' ? media.picture : media.video;
+    }
+  }
+
+  get media() {
+    return `assets/images/portfolios/fullsize/${this.photographerId}/`;
+  }
+}
