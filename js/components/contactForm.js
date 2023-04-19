6486a465c1712b6625e1413aa4d365fad926af5c
Richard Ogden <ogden.richard@gmail.com>
2023-04-19 17:00:15 +0200

    19042023 | ContactForm.js

--- js/components/contactForm.js
+++ js/components/contactForm.js
@@ -1,9 +1,38 @@
-function displayModal() {
-  const modal = document.getElementById('background_modal');
-  modal.style.display = 'block';
-}
+export default class ContactForm {
+  constructor(photographer) {
+    this.name = photographer.name;
+    this.form = document.getElementById('modalform');
+    this.headline = document.getElementById('contact-headline');
+    this.contactButton = document.getElementById('launchform');
+    this.closeButton = document.getElementById('closeform');
+    this.background = document.getElementById('background_modal');
+    this.submit = document.getElementById('sendform');
+    this.buildHeadline();
+    this.disableForm();
+    this.displayModal();
+    this.closeModal();
+  }
 
-function closeModal() {
-  const modal = document.getElementById('background_modal');
-  modal.style.display = 'none';
+  buildHeadline = () => {
+    this.headline.innerText += `
+    ${this.name}`;
+  };
+
+  displayModal = () => {
+    this.contactButton.addEventListener('click', () => {
+      this.background.style.display = 'block';
+    });
+  };
+
+  closeModal = () => {
+    this.closeButton.addEventListener('click', () => {
+      this.background.style.display = 'none';
+    });
+  };
+
+  disableForm = () => {
+    this.form.addEventListener('submit', (e) => {
+      e.preventDefault();
+    });
+  };
 }
--- js/pages/photographer.js
+++ js/pages/photographer.js
@@ -4,6 +4,7 @@ import PhotographerModel from '../models/PhotographerModel.js';
 import GridSubject from '../observers/GridSubject.js';
 import Dropdown from '../components/Dropdown.js';
 import GridBuilder from '../components/GridBuilder.js';
+import ContactForm from '../components/contactForm.js';
 
 async function getPhotographer() {
   const params = new URL(document.location).searchParams;
@@ -37,6 +38,7 @@ function displayPhotographHeader(photographer) {
 async function init() {
   const main = document.getElementById('main');
   const { photographer, media } = await getPhotographer();
+  const contactForm = new ContactForm(photographer[0]);
   const gridSubject = new GridSubject();
   const portfolio = new GridBuilder(media, gridSubject);
   const dropdown = new Dropdown(portfolio);
