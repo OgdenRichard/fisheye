c084518da958f967d6151043873bab1e76e1068f
Richard Ogden <ogden.richard@gmail.com>
2023-05-02 16:56:26 +0200

    02052023 | dropdown > swap data-filters & filters working

--- js/components/Dropdown.js
+++ js/components/Dropdown.js
@@ -18,51 +18,16 @@ export default class Dropdown {
   setFilterListener = (filter) => {
     filter.addEventListener('click', () => {
       const swaptext = filter.firstChild.textContent;
+      const dataFilter = filter.dataset.filter;
       filter.firstChild.textContent = this.trigger.textContent;
+      filter.dataset.filter = this.trigger.dataset.filter;
       this.trigger.firstChild.textContent = swaptext;
-      document.activeElement.blur();
+      this.trigger.dataset.filter = dataFilter;
+      filter.blur();
+      this.gridBuilder.sortBy = dataFilter;
+      this.gridBuilder.refresh();
     });
   };
-
-  /* setFilterListener = (filter) => {
-    let display = 'none';
-    filter.addEventListener('click', () => {
-      let prevElement = filter.previousElementSibling;
-      let nextElement = filter.nextElementSibling;
-      this.expanded = !this.expanded;
-      this.trigger.ariaExpanded = `${this.expanded}`;
-      if (this.expanded) {
-        this.arrow.classList.add('arrow-down');
-      } else {
-        this.arrow.classList.remove('arrow-down');
-      }
-      display = this.expanded ? 'block' : 'none';
-      this.toggleVideoPointer(this.expanded);
-      if (!filter.classList.contains('filter-active')) {
-        const text = filter.textContent;
-        const dataFilter = filter.dataset.filter;
-        filter.style.display = display;
-        filter.textContent = this.trigger.textContent;
-        filter.dataset.filter = this.trigger.dataset.filter;
-        this.trigger.dataset.filter = dataFilter;
-        this.trigger.textContent = text;
-        this.trigger.appendChild(this.arrow);
-        this.gridBuilder.sortBy = dataFilter;
-        this.gridBuilder.refresh();
-      }
-      if (nextElement) {
-        while (nextElement) {
-          nextElement.style.display = display;
-          nextElement = nextElement.nextElementSibling;
-        }
-      } else if (prevElement) {
-        while (prevElement && prevElement.classList.contains('filter-toggle')) {
-          prevElement.style.display = display;
-          prevElement = prevElement.previousElementSibling;
-        }
-      }
-    });
-  }; */
 
   toggleVideoPointer = () => {
     this.gridBuilder.gridElements[0].togglePointerEvents(this.expanded);
